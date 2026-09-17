73b921af0ec22909aee6d2b131bf9f8dfa3367d7
Felipe Caldeira Akryghti <felipecaldeira2804@gmail.com>
2026-09-17 18:15:03 -0300

    Implementação da main

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	r := gin.New()
 
 	// Uso dos Middlewares globais nativos e personalizados
+	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
 	// 4. Mapeamento de Rotas sob Grupo Versionado
@@ -26,10 +27,27 @@ func main() {
 			})
 		})
 
+		// Domínio de Salas
+		v1.POST("/salas", criarSala)
+		v1.GET("/salas", listarSalas)
+		v1.PUT("/salas/:id", atualizarSala)
+		v1.DELETE("/salas/:id", excluirSala)
+
+		// Domínio de Alunos
+		v1.POST("/alunos", criarAluno)
+		v1.GET("/alunos", listarAlunos)
+		v1.GET("/alunos/:matricula", buscarAluno)
+		v1.PUT("/alunos/:matricula", atualizarAluno)
+		v1.DELETE("/alunos/:matricula", excluirAluno)
+
 		// Domínio de Turmas (Classes)
-		//v1.POST("/turmas", turmaHandler.CriarTurma)
-		//v1.GET("/turmas", turmaHandler.ListarTurmas)
-		//v1.POST("/turmas/:id/alocar", turmaHandler.AlocarSala)
+		v1.POST("/turmas", criarTurma)
+		v1.GET("/turmas", listarTurmas)
+		v1.PUT("/turmas/:id", atualizarTurma)
+		v1.DELETE("/turmas/:id", excluirTurma)
+		v1.POST("/turmas/:id/alunos", matricularAluno)
+		v1.GET("/turmas/:id/alunos", listarAlunosDaTurma)
+		v1.POST("/turmas/:id/alocar", alocarSala)
 	}
 
 	r.Run(":8080")
